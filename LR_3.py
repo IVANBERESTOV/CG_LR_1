dc16914c48d49db005c4fc55ddc6c8b8d0402a65
Ivan_Berestov_notebook <bim@smr>
2025-05-03 02:15:42 +0400

    new F1 test added

--- LR_3.py
+++ LR_3.py
@@ -97,18 +97,12 @@ def objParser(file_str):
                 pol.append(polOne)
     return ver, pol
 
-def paint(matrix, color, ver, pol=0,shadow=False):
-    for i in ver:
-        x=int(i[0])
-        y=int(i[1])
-        matrix[y][x]=color
+def paint(matrix, zbuf, color, ver, pol=0,shadow=False):
     if(pol!=0):
         for i in pol:
-            temp = [0] * 2
-            for j in range(0,4):
-                temp[1]=i[j%3]
-                if(temp[0]):Brithenham(matrix, int(ver[temp[0]-1][0]), int(ver[temp[0]-1][1]),int(ver[temp[1]-1][0]),int(ver[temp[1]-1][1]), color if (not shadow)else ver[temp[0]-1][2])
-                temp[0]=temp[1]
+            temp=cut(ver[i[0]-1][0],ver[i[0]-1][1],ver[i[0]-1][2],ver[i[1]-1][0],ver[i[1]-1][1],ver[i[1]-1][2],ver[i[2]-1][0],ver[i[2]-1][1],ver[i[2]-1][2])
+            if(temp<0):
+                draw_tr(matrix, zbuf, ver[i[0]-1][0],ver[i[0]-1][1],ver[i[0]-1][2],ver[i[1]-1][0],ver[i[1]-1][1],ver[i[1]-1][2],ver[i[2]-1][0],ver[i[2]-1][1],ver[i[2]-1][2],-255*temp)
 
 def getMatrix(H, W, color):
     matrix = np.zeros((W, H, 3), dtype=np.uint8)
@@ -117,58 +111,85 @@ def getMatrix(H, W, color):
             matrix[i][j] = color
     return matrix
 
+def getZBuff(H,W):
+    matrix = np.zeros((W,H),dtype=np.float64)
+    for i in range(0, W):
+        for j in range(0, H):
+            matrix[i][j]=np.inf
+    return matrix
+
 def saveMatrixAsIMG(matrix):
     image = Image.fromarray(matrix, mode='RGB')
     image = ImageOps.flip(image)
     image.save('image1.png')
 
-def resize(W, H, ver, zShadow=False, color=0, shadowIntens=0):
-    xMax,yMax,zMax,xMin,yMin,zMin = ver[0][0],ver[0][1],ver[0][2],ver[0][0],ver[0][1],ver[0][2]
+def rotMatrix(alpha=np.radians(0),beta=np.radians(0),gamma=np.radians(0)):
+    Rx=np.array(
+        [[1,0,0],
+        [0, np.cos(alpha),np.sin(alpha)],
+        [0,-np.sin(alpha),np.cos(alpha)]]
+    )
+
+    Ry=np.array(
+        [[np.cos(beta),0,np.sin(beta)],
+        [0, 1,0],
+        [-np.sin(beta),0,np.cos(beta)]]
+    )
+
+    Rz=np.array(
+        [[np.cos(gamma), np.sin(gamma),0],
+        [-np.sin(gamma),np.cos(gamma),0],
+        [0, 0, 1]]
+    )
+    return np.dot(Rx,np.dot(Ry,Rz))
+
+def resize(ver,tx=0,ty=0,tz=0,alpha=np.radians(0),beta=np.radians(0),gamma=np.radians(0),a=4500):
     for i in ver:
-        if(i[0]>xMax):xMax=i[0]
-        if(i[0]<xMin):xMin=i[0]
-        if(i[1]>yMax):yMax=i[1]
-        if(i[1]<yMin):yMin=i[1]
-        if(i[2]>zMax):zMax=i[2]
-        if(i[2]<zMin):zMin=i[2]
-    absX=abs(xMax-xMin)
-    absY=abs(yMax-yMin)
-    absZ=abs(zMax-zMin)
-    if(absX<0.0001):absX=0.0001
-    if(absY<0.0001):absY=0.0001
-    if(absZ<0.0001):absZ=0.0001
-
-    coefX=int((W-10)/absX)
-    coefY=int((H-10)/absY)
-    
-    cenX=int(coefX*(xMax+xMin)/2)
-    cenY=int(coefY*(yMax+yMin)/2)
-    cenW=W/2
-    cenH=H/2
-    pointsToCenterW=cenW-cenX
-    pointsToCenterH=cenH-cenY
-    
-    
-    if(zShadow):
-        shadowIntens
-        coefZR=int((color[0]-int(color[0]/shadowIntens))/absZ)
-        coefZG=int((color[1]-int(color[1]/shadowIntens))/absZ)
-        coefZB=int((color[2]-int(color[2]/shadowIntens))/absZ)
-        pointsToZeroZR=int(color[0]/shadowIntens)-int(zMin*coefZR)
-        pointsToZeroZG=int(color[1]/shadowIntens)-int(zMin*coefZG)
-        pointsToZeroZB=int(color[2]/shadowIntens)-int(zMin*coefZB)
-        for i in ver:
-            i[0],i[1],i[2] = i[0]*coefX+pointsToCenterW,i[1]*coefY+pointsToCenterH,(int(i[2]*coefZR+pointsToZeroZR),int(i[2]*coefZG+pointsToZeroZG),int(i[2]*coefZB+pointsToZeroZB))
-    else:
-        for i in ver:
-            i[0],i[1] = i[0]*coefX+pointsToCenterW,i[1]*coefY+pointsToCenterH
+        i[0],i[1],i[2] = np.dot(rotMatrix(alpha,beta,gamma),[i[0], i[1], i[2]]) + [tx,ty,tz]
+    for i in ver:
+        i[0],i[1] =  a*i[0]+W/2,a*i[1]+H/2
     return ver
+   
 
+def bary(x0,y0,x1,y1,x2,y2,x,y):
+    l0=((x - x2) * (y1 - y2) - (x1 - x2) * (y - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
+    l1=((x0 - x2) * (y - y2) - (x - x2) * (y0 - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
+    l2 = 1.0 - l0 - l1
+    return l0, l1, l2
+
+def draw_tr(matrix,zbuf,x0,y0,z0,x1,y1,z1,x2,y2,z2,color=0):
+    xmin=min(x0,x1,x2)
+    xmax=max(x0,x1,x2)
+    ymin=min(y0,y1,y2)
+    ymax=max(y0,y1,y2)
+    if(xmin<0):xmin=0
+    if(ymin<0):ymin=0
+    xmin=int(m.floor(xmin))
+    xmax=int(m.ceil(xmax))
+    ymin=int(m.floor(ymin))
+    ymax=int(m.ceil(ymax))
+    for x in range(xmin,xmax+1):
+        for y in range(ymin,ymax+1):
+            l0,l1,l2 = bary(x0,y0,x1,y1,x2,y2,x,y)
+            if(l0>=0 and l1>=0 and l2>=0):
+                z=l0*z0+l1*z1+l2*z2
+                if(z < zbuf[y][x]):
+                    matrix[y][x]=color
+                    zbuf[y][x] = z
+
+def nor(x0,y0,z0,x1,y1,z1,x2,y2,z2):
+    return np.cross(np.array([x1-x2,y1-y2,z1-z2]),np.array([x1-x0,y1-y0,z1-z0]))
+
+def cut(x0,y0,z0,x1,y1,z1,x2,y2,z2):
+    l=[0,0,1]
+    var = np.dot(nor(x0,y0,z0,x1,y1,z1,x2,y2,z2),l)/(np.linalg.norm(nor(x0,y0,z0,x1,y1,z1,x2,y2,z2))*np.linalg.norm(l))
+    return var
 
 W = 1600
 H = 900
 matrix = getMatrix(W, H,(0, 0, 0))
 ver, pol = objParser("model_1.obj")
-ver=resize(W, H, ver,True,(255,255,255),250)
-paint(matrix, (255,255,255), ver, pol, True)
+ver=resize(ver,0,0,0,np.radians(0),np.radians(0),np.radians(0))
+zbuf=getZBuff(W,H)
+paint(matrix,zbuf,255,ver,pol)
 saveMatrixAsIMG(matrix)
---
+++ LR_5.py
@@ -0,0 +1,158 @@
+import numpy as np
+import math as m
+from PIL import Image,ImageOps;
+
+def objParser(file_str):
+    ver = []
+    pol = []
+    vt = []
+    with open(file_str, 'r') as f:
+        for line in f:
+            s=line.split(' ')
+            if(s[0] == 'v'):
+                ver.append([float(x) for x in s[1:4]])
+            if(s[0] == 'f'):
+                polOne = [0] * (6)
+                curPoint=2
+                for i in range(0,len(s)):
+                    if(i%3==0):
+                        if(polOne[2]!=0):
+                            pol.append(polOne)
+                        polOne[0]=int(s[1].split('/')[0])
+                        polOne[3]=int(s[1].split('/')[1])
+                    else:
+                        polOne[i%3]=int(s[curPoint].split('/')[0])
+                        polOne[i%3+3]=int(s[curPoint].split('/')[1])
+                        curPoint+=1
+            if(s[0] == 'vt'):
+                vt.append([float(x) for x in s[1:3]])
+    return ver, pol, vt
+
+def getNormArr(ver, pol, normArr):
+    for i in pol:
+        temp =  nor(ver[i[0]-1][0],ver[i[0]-1][1],ver[i[0]-1][2],ver[i[1]-1][0],ver[i[1]-1][1],ver[i[1]-1][2],ver[i[2]-1][0],ver[i[2]-1][1],ver[i[2]-1][2])
+        temp/=np.linalg.norm(temp)
+        normArr[i[0]-1] += temp
+        normArr[i[1]-1] += temp
+        normArr[i[2]-1] += temp
+    for j in range(0,len(normArr)):
+        normArr[j] = normArr[j]/np.linalg.norm(normArr[j])
+    return normArr
+
+def paint(matrix, zbuf, cameraCoef, sizeCoef, ver, pol, normArr,vt):
+    if(pol!=0):
+        for i in pol:
+            temp=cut(ver[i[0]-1][0],ver[i[0]-1][1],ver[i[0]-1][2],ver[i[1]-1][0],ver[i[1]-1][1],ver[i[1]-1][2],ver[i[2]-1][0],ver[i[2]-1][1],ver[i[2]-1][2])
+            if(temp<0):
+                draw_tr(matrix, zbuf, cameraCoef, sizeCoef, ver[i[0]-1][0],ver[i[0]-1][1],ver[i[0]-1][2],ver[i[1]-1][0],ver[i[1]-1][1],ver[i[1]-1][2],ver[i[2]-1][0],ver[i[2]-1][1],ver[i[2]-1][2],normArr[i[0]-1],normArr[i[1]-1],normArr[i[2]-1],i,vt)
+
+def getMatrix(H, W, color):
+    matrix = np.zeros((W, H, 3), dtype=np.uint8)
+    for i in range(0, W):
+        for j in range(0, H):
+            matrix[i][j] = color
+    return matrix
+
+def getZBuff(H,W):
+    matrix = np.zeros((W,H),dtype=np.float64)
+    for i in range(0, W):
+        for j in range(0, H):
+            matrix[i][j]=np.inf
+    return matrix
+
+def saveMatrixAsIMG(matrix):
+    image = Image.fromarray(matrix, mode='RGB')
+    image = ImageOps.flip(image)
+    image.save('image1.png')
+
+def rotMatrix(alpha=np.radians(0),beta=np.radians(0),gamma=np.radians(0)):
+    Rx=np.array(
+        [[1,0,0],
+        [0, np.cos(alpha),np.sin(alpha)],
+        [0,-np.sin(alpha),np.cos(alpha)]]
+    )
+
+    Ry=np.array(
+        [[np.cos(beta),0,np.sin(beta)],
+        [0, 1,0],
+        [-np.sin(beta),0,np.cos(beta)]]
+    )
+
+    Rz=np.array(
+        [[np.cos(gamma), np.sin(gamma),0],
+        [-np.sin(gamma),np.cos(gamma),0],
+        [0, 0, 1]]
+    )
+    return np.dot(Rx,np.dot(Ry,Rz))
+
+def resize(ver,tx=0,ty=0,tz=0,alpha=np.radians(0),beta=np.radians(0),gamma=np.radians(0)):
+    for i in ver:
+        i[0],i[1],i[2] = np.dot(rotMatrix(alpha,beta,gamma),[i[0], i[1], i[2]]) + [tx,ty,tz]
+    return ver
+   
+
+def bary(x0,y0,x1,y1,x2,y2,x,y):
+    l0=((x - x2) * (y1 - y2) - (x1 - x2) * (y - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
+    l1=((x0 - x2) * (y - y2) - (x - x2) * (y0 - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
+    l2 = 1.0 - l0 - l1
+    return l0, l1, l2
+
+def draw_tr(matrix,zbuf,cameraCoef,sizeCoef,x0,y0,z0,x1,y1,z1,x2,y2,z2,i0,i1,i2,ipol,vt):
+    # Сюда добавляем
+    a = 7000*cameraCoef*sizeCoef
+    px0, py0 =  (a * x0 / z0 + W/2), (a * y0 /z0 + H/2)
+    px1, py1 =  (a * x1 / z1 + W/2), (a * y1 / z1 + H/2)
+    px2, py2 =  (a * x2 / z2 + W/2), (a * y2 / z2 + H/2)
+    
+    I0=i0[2]
+    I1=i1[2]
+    I2=i2[2]
+
+    p0ResCoord=(vt[ipol[3]-1][1], vt[ipol[3]-1][0])
+    p1ResCoord=(vt[ipol[4]-1][1], vt[ipol[4]-1][0])
+    p2ResCoord=(vt[ipol[5]-1][1], vt[ipol[5]-1][0])
+
+    # И везде дальше меняем x на px
+    xmin=min(px0,px1,px2)
+    xmax=max(px0,px1,px2)
+    ymin=min(py0,py1,py2)
+    ymax=max(py0,py1,py2)
+    if(xmin<0):xmin=0
+    if(ymin<0):ymin=0
+    xmin=int(m.floor(xmin))
+    xmax=int(m.ceil(xmax))
+    ymin=int(m.floor(ymin))
+    ymax=int(m.ceil(ymax))
+    for x in range(xmin,xmax+1):
+        for y in range(ymin,ymax+1):
+            l0,l1,l2 = bary(px0,py0,px1,py1,px2,py2,x,y)
+            if(l0>=0 and l1>=0 and l2>=0):
+                z=l0*z0+l1*z1+l2*z2
+                if(z < zbuf[y%H][x%W]):
+                    I = l0*I0+l1*I1+l2*I2
+                    color = (-I) * txtres[int(txtres.shape[0]*(l0*p0ResCoord[0]+l1*p1ResCoord[0]+l2*p2ResCoord[0]))][int(txtres.shape[1]*(l0*p0ResCoord[1]+l1*p1ResCoord[1]+l2*p2ResCoord[1]))]
+                    matrix[y%H][x%W] = color
+                    zbuf[y%H][x%W] = z
+
+def nor(x0,y0,z0,x1,y1,z1,x2,y2,z2):
+    return np.cross(np.array([x1-x2,y1-y2,z1-z2]),np.array([x1-x0,y1-y0,z1-z0]))
+
+def cut(x0,y0,z0,x1,y1,z1,x2,y2,z2):
+    l=[0,0,1]
+    var = np.dot(nor(x0,y0,z0,x1,y1,z1,x2,y2,z2),l)/(np.linalg.norm(nor(x0,y0,z0,x1,y1,z1,x2,y2,z2))*np.linalg.norm(l))
+    return var
+
+W = 1600
+H = 900
+cameraCoef=0.17
+sizeCoef=1
+txtres = np.array(ImageOps.flip(Image.open("bunny-atlas.jpg")))
+print(txtres.shape)
+matrix = getMatrix(W, H,(0, 0, 0))
+ver, pol , vt = objParser("model_1.obj")
+normArr = np.zeros((len(ver), 3))
+ver=resize(ver,0,0,-cameraCoef, np.radians(0),np.radians(-90),np.radians(0))
+normArr=getNormArr(ver,pol,normArr)
+zbuf=getZBuff(W,H)
+paint(matrix,zbuf,cameraCoef,sizeCoef,ver,pol,normArr,vt)
+saveMatrixAsIMG(matrix)
